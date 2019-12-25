e976d953227525e44422a59e2009ada41c24b035
Kyle Lynch <kylelynch104@gmail.com>
2019-12-25 17:18:55 -0600

    drawer navigator

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,17 +1,27 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import Home from '../screens/Home';
-import GlitchDetails from '../screens/GlitchDetails';
+import { createStackNavigator } from "react-navigation-stack";
+import Home from "../screens/Home";
+import GlitchDetails from "../screens/GlitchDetails";
 
 const screens = {
   Home: {
-    screen: Home 
+    screen: Home,
+    navigationOptions: {
+      title: "GlitchPost"
+    }
   },
   GlitchDetails: {
-    screen: GlitchDetails
+    screen: GlitchDetails,
+    navigationOptions: {
+      title: "Glitch Getails"
+    }
   }
-}
+};
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: "#eee", height: 60 }
+  }
+});
 
-export default createAppContainer(HomeStack)
+export default HomeStack;
