f8a0fc31bd993f531de956b944c97226d1238eef
Kyle Lynch <kylelynch104@gmail.com>
2020-01-10 12:17:41 -0600

    add modal

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
-import Card from "../shared/card";
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from "react-native";
 import { globalStyles } from "../styles/global";
+import { MaterialIcons } from "@expo/vector-icons";
+import Card from "../shared/card";
 
 const Home = ({ navigation }) => {
+  const [modalOpen, setModalOpen] = useState(false);
   const [glitches, setGlitches] = useState([
     {
       game: "GTAV",
@@ -33,7 +35,23 @@ const Home = ({ navigation }) => {
 
   return (
     <View style={globalStyles.container}>
-      {/* <Text style={globalStyles.mainText}>Glitches/Methods</Text> */}
+      <Modal visible={modalOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+        <MaterialIcons 
+        name="close" 
+        size={24} 
+        style={{ ...styles.modalToggle, ...styles.modalClose }} 
+        onPress={() => setModalOpen(false)} 
+        />
+          <Text>Hello from Modal!</Text>
+        </View>
+      </Modal>
+      <MaterialIcons 
+      name="add" 
+      size={24} 
+      style={styles.modalToggle}
+      onPress={() => setModalOpen(true)} 
+      />
       <FlatList
         data={glitches}
         renderItem={({ item }) => (
@@ -50,5 +68,23 @@ const Home = ({ navigation }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    padding: 10,
+    alignSelf: 'center'
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0
+  },
+  modalContent: {
+    flex: 1
+  }
+})
 
 export default Home;
